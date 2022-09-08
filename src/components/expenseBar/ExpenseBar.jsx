8e4e7136da5b6fd5c3e08b9ff45c3aba6882d51d
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-09-09 00:37:18 +0300

    Designing list visual format

--- src/components/expenseBar/ExpenseBar.jsx
+++ src/components/expenseBar/ExpenseBar.jsx
@@ -2,21 +2,32 @@ import React from 'react';
 
 export default function ExpenseBar() {
   return (
-    <div className="expense-bar">
-      <div className="selection-list">
-        <select>
-          {/* <option value="0">Select</option> */}
-          <option value="" defaultValue={'selected'} disabled hidden>
-            Select
-          </option>
-          <option value="1">Food</option>
-          <option value="2">Houseware</option>
-          <option value="3">Entertainment</option>
-        </select>
+    <>
+      <div className="expense-bar">
+        <div className="selection-list">
+          <select>
+            <option value="" selected disabled hidden>
+              Select
+            </option>
+            <option value="1">Food</option>
+            <option value="2">Houseware</option>
+            <option value="3">Entertainment</option>
+          </select>
+        </div>
+        <form>
+          <button className="button-style">Add expense</button>
+        </form>
       </div>
-      <form>
-        <button className="button-style">Add expense</button>
-      </form>
-    </div>
+      <div className="item-list">
+        <form>
+          <input type="text" placeholder="Example item" />
+          <input type="text" placeholder="€0.00" />
+        </form>
+        <div className="total-items">
+          <p>Total</p>
+          <p>€0.00</p>
+        </div>
+      </div>
+    </>
   );
 }
