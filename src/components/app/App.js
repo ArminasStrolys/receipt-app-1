26b466bdbc2a53cf88c05b2125b320e4e771c996
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-09-10 14:23:58 +0300

    Fixed fonts
    Noticed the fonts weren't similar to ones that are in the design. Imported fonts the correct way.

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import MainBar from '../mainBar/MainBar';
+// import bold from '../../fonts/HKGrotesk-Bold.otf'
+// import regular from '../../fonts/HKGrotesk-Medium.otf'
+// import semibold from '../../fonts/HKGrotesk-SemiBold.otf'
 
 function App() {
   return (
